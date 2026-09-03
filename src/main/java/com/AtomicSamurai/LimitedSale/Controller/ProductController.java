cc90880634f435f5517d68ed747857e984547b2b
Adithya <adithyapawar07@gmail.com>
2026-09-03 23:27:40 +0530

    Added Product Controller

---
+++ src/main/java/com/AtomicSamurai/LimitedSale/Controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.AtomicSamurai.LimitedSale.Controller;
+
+
+import com.AtomicSamurai.LimitedSale.Service.ProductService;
+import com.AtomicSamurai.LimitedSale.dto.CreateProductRequest;
+import com.AtomicSamurai.LimitedSale.dto.ProductResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody CreateProductRequest request) {
+        return ResponseEntity.ok(productService.createProduct(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+}
