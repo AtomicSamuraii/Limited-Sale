89714b7e5b667806fbf47764570e2a8d8c360872
Adithya <adithyapawar07@gmail.com>
2026-09-04 23:14:26 +0530

    Added Global Exception handling

---
+++ src/main/java/com/AtomicSamurai/LimitedSale/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.AtomicSamurai.LimitedSale.Exception;
+
+
+import com.AtomicSamurai.LimitedSale.dto.ExceptionResponse;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleEntityNotFound(EntityNotFoundException ex){
+        ExceptionResponse response = new ExceptionResponse(404, ex.getMessage(), LocalDateTime.now());
+
+        return ResponseEntity.status(404).body(response);
+    }
+}
